88dbff249dd8872dc59edd8e4cd2b50f8c081b3e
Ruan Freire <159321252+FreireRuan@users.noreply.github.com>
2025-05-13 00:36:44 -0300

    fix send_to_slack.py

--- scripts/send_to_slack.py
+++ scripts/send_to_slack.py
@@ -1,28 +1,31 @@
-from slack_sdk import WebClient
+import slack_sdk
 from slack_sdk.errors import SlackApiError
 import os
 from dotenv import load_dotenv
+from datetime import date
 
 load_dotenv(dotenv_path="credencial.env")
 
 SLACK_TOKEN = os.getenv("SLACK_TOKEN")
 SLACK_CHANNEL = os.getenv("SLACK_CHANNEL")
 
-mensagem = """
-Arquivo enviado com sucesso para o Google Drive!
-Acesse o link para visualizar: https://drive.google.com/drive/folders/1UXUP3M_6Ik0zV__80Yx6HsfGrK1Uhiwv?usp=sharing
-"""
+client = slack_sdk.WebClient(SLACK_TOKEN)
 
-def send_message():
-    client = WebClient(token=SLACK_TOKEN)
-    try:
-        response = client.chat_postMessage(
-            channel=SLACK_CHANNEL,
-            text=mensagem
-        )
-        print("✅ Mensagem enviada com sucesso.")
-    except SlackApiError as e:
-        print(f"❌ Erro ao enviar mensagem: {e.response['error']}")
+# data de hoje
+hoje = date.today().strftime("%Y-%m-%d")
 
-if __name__ == "__main__":
-    send_message()
+# saída do arquivo
+file_path = f"agendamentos_{hoje}.csv"
+
+try:
+
+    response = client.files_upload_v2(
+        file=file_path,
+        title="Arquivo csv",
+        channel=SLACK_CHANNEL,
+        initial_comment="Arquivo com os agendamentos no dia de hoje",
+    )
+
+# TRATAMENTO DE EXCECAO
+except Exception as e:
+    print(f"Error: {e}")
